2dc57259a0eb820f5aad8e80b8a53a1edaf9062a
KexySerJ90 <exserj9011@gmail.com>
2023-12-05 18:52:34 +0300

    password restore begin

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register', views.register, name='register'),
@@ -11,7 +12,12 @@ urlpatterns = [
     path('user-logout', views.user_logout, name='user-logout'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('profile-management', views.profile_management, name='profile-management'),
-    path('delete-account', views.delete_account, name='delete-account' )
+    path('delete-account', views.delete_account, name='delete-account' ),
+
+    path('reset_password', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(), name='reset_password_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(), name='reset_password_confirm'),
+    path('reset_password_comple', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
 
 
 ]
