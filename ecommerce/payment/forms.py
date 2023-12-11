b119ebb60a58992fa56bdc65f12a109a60bf7e60
KexySerJ90 <exserj9011@gmail.com>
2023-12-11 23:49:51 +0300

    middle stripe integration

--- ecommerce/payment/forms.py
+++ ecommerce/payment/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.validators import EmailValidator
+
 from .models import ShippingAdress
 
 class ShippingForm(forms.ModelForm):
@@ -6,3 +8,11 @@ class ShippingForm(forms.ModelForm):
         model=ShippingAdress
         fields = ['full_name', 'email', 'address1', 'address2', 'city', 'state', 'zipcode']
         exclude=['user',]
+        labels = {'full_name': 'Full name*','email':'Email*', 'address1': 'Address1*','address2': 'Address2*', 'city':'City*' }
+        widgets = {
+            field: forms.TextInput(attrs={"class":"form-control validate", 'placeholder': field.capitalize(), 'autocomplete':'off'}) for field in fields
+            if field != 'email'
+        }
+        widgets['email'] = forms.EmailInput(attrs={"class": "form-control validate", 'placeholder': 'Email', 'autocomplete':'off'})
+
+
--- ecommerce/payment/views.py
+++ ecommerce/payment/views.py
@@ -1,20 +1,35 @@
+from decimal import Decimal
+
+import stripe
+from django import forms
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .forms import ShippingForm
 from .models import ShippingAdress, Order, OrderItem
 from cart.cart import Cart
 
+stripe.api_key=settings.STRIPE_SECRET_KEY
+stripe.api_version=settings.STRIPE_API_VERSION
 
 # Create your views here.
 def checkout(request):
     if request.user.is_authenticated:
         try:
-            shipping_address = ShippingAdress.objects.get(user=request.user.id)
-            context = {'shipping': shipping_address}
+            shipping_address = ShippingAdress.objects.get(user=request.user)
+            form = ShippingForm(instance=shipping_address)
+            context = {'form': form}
             return render(request, 'payment/checkout.html', context=context)
-        except:
-            return render(request, 'payment/checkout.html')
+        except ShippingAdress.DoesNotExist:
+            form = ShippingForm()
+            context = {'form': form}
+            return render(request, 'payment/checkout.html', context=context)
     else:
-        return render(request, 'payment/checkout.html')
+        form = ShippingForm()
+        context = {'form': form}
+        return render(request, 'payment/checkout.html', context=context)
 
 
 def payment_success(request):
@@ -29,35 +44,67 @@ def payment_failed(request):
 
 
 def complete_order(request):
-    if request.POST.get('action') == 'post':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        address1 = request.POST.get('address1')
-        address2 = request.POST.get('address2')
-        city = request.POST.get('city')
-        state = request.POST.get('state')
-        zipcode = request.POST.get('zipcode')
-
-        shipping_address = (address1 + '\n' + address2 + '\n' + city + '\n' + state + '\n' + zipcode)
-
+    if request.method =='POST':
+        form=ShippingForm(request.POST)
+        payment_type = request.POST.get('stripe-payment')
         cart = Cart(request)
         total_cost = cart.get_total()
+        if form.is_valid():
+            full_name = form.cleaned_data['full_name']
+            email = form.cleaned_data['email']
+            address1 = form.cleaned_data['address1']
+            address2 = form.cleaned_data['address2']
+            city = form.cleaned_data['city']
+            state = str(form.cleaned_data['state'])
+            zipcode = str(form.cleaned_data['zipcode'])
+            shipping_address = (address1 + '\n' + address2 + '\n' + city + '\n' + state + '\n' + zipcode)
+            match payment_type:
+                case 'stripe-payment':
+                    session_data ={
+                        'mode': 'payment',
+                        'success_url': request.build_absolute_uri(reverse('payment-success')),
+                        'cancel_url': request.build_absolute_uri(reverse('payment-failed')),
+                        'line_items':[]
+                    }
 
-        if request.user.is_authenticated:
-            order = Order.objects.create(full_name=name, email=email, shipping_address=shipping_address,
-                                         amount_paid=total_cost, user=request.user)
-            order_id = order.pk
-            for item in cart:
-                OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'],
-                                         price=item['price'], user=request.user)
+                    if request.user.is_authenticated:
+                        order = Order.objects.create(full_name=full_name, email=email, shipping_address=shipping_address,
+                                                     amount_paid=total_cost, user=request.user)
+                        order_id = order.id
+                        for item in cart:
+                            OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'],
+                                                     price=item['price'], user=request.user)
+                            session_data['line_items'].append({
+                                'price_data': {
+                                    'unit_amount': int(item['price']* Decimal(100)),
+                                    'currency':'usd',
+                                    'product_data': {
+                                        'name': item['product']
+                                    },
+                                },
+                                'quantity': item['qty'],
+                            })
+                        session_data['client_reference_id'] = order.id
+                        session= stripe.checkout.Session.create(**session_data)
+                        return redirect(session.url, code=303)
 
-        else:
-            order = Order.objects.create(full_name=name, email=email, shipping_address=shipping_address,
-                                         amount_paid=total_cost)
-            order_id = order.pk
-            for item in cart:
-                OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'],
-                                         price=item['price'])
-        order_success=True
-        response=JsonResponse({'success':order_success})
-        return response
+                    else:
+                        order = Order.objects.create(full_name=full_name, email=email, shipping_address=shipping_address,
+                                                     amount_paid=total_cost)
+                        order_id = order.pk
+                        for item in cart:
+                            OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'],
+                                                     price=item['price'])
+                            session_data['line_items'].append({
+                                'price_data': {
+                                    'unit_amount': int(item['price'] * Decimal(100)),
+                                    'currency': 'usd',
+                                    'product_data': {
+                                        'name': item['product']
+                                    },
+                                },
+                                'quantity': item['qty'],
+                            })
+                        session_data['client_reference_id'] = order.id
+                        session = stripe.checkout.Session.create(**session_data)
+                        return redirect(session.url, code=303)
